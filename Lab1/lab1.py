4a3689d47033c42b9eba8bd34dd14a0873b6bae0
IHmel <pro100.hmel@gmail.com>
2023-09-29 16:17:09 +0300

    изменено:      Lab1/lab1.py 	изменено:      Lab2/Lab2.py

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -2,10 +2,7 @@ import hashlib
 import cryptography
 from cryptography.fernet import Fernet
 ################################################################
-def generate_key():
-    key = Fernet.generate_key()
-    with open('key.key', 'wb') as key_file:
-        key_file.write(key)
+
 #Класс движка
 
 class console:
@@ -13,7 +10,6 @@ class console:
     user_list = {}
     key = str
     def read_file(self):
-        self.key = open('key.key', 'rb').read()
         with open('users.txt') as file: #Читаем файл
             lines = file.read().splitlines() # read().splitlines()
         for line in lines: # Проходимся по каждой строчке
@@ -94,7 +90,6 @@ class console:
 
 
 if __name__ == '__main__':
-    #generate_key()
     Cons = console()
     Cons.read_file()
     while(True):
