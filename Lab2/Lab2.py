28f45e3ecd9b45d7f127c6556f34e76d24ac018f
IHmel <pro100.hmel@gmail.com>
2023-09-29 21:52:20 +0300

    modified:   Lab2/Lab2.py

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -25,8 +25,9 @@ class Database():
                         Штатное расписание TEXT
                         )
                         ''')
+        self.con.commit()
 
-    def add_struct(self):
+    def add_user(self):
         cur = self.con.cursor()
         print('Введите СНИЛС нового сотрудника или 0 для отмены')
         snils = input()
@@ -40,6 +41,7 @@ class Database():
                 if choice == '0':
                     flag = False
                     cur.execute('INSERT INTO Users VALUES (?,?,?,?,?,?)', data)
+                    self.con.commit()
                 if choice == '1':
                     data[0] = input('Введите ФИО')
                 if choice == '2':
@@ -51,3 +53,22 @@ class Database():
                 if choice == '5':
                     data[4] = input('Введите Датe приема')
                 
+    def add_structur(self):
+        cur = self.con.cursor()
+        print('Введите название новой структуры или 0 для отмены')
+        name = input()
+        if name == '0':
+            print('Отменено')
+        else:
+            data = [name, 'NULL', 'NULL']
+            flag = True
+            while flag:
+                choice = input('Выберете данные для ввода'+ '\n1 - Описание'+'\n2 - Штатное расписание'+'\n0 - Для сохранения'+'\n')
+                if choice == '0':
+                    flag = False
+                    cur.execute('INSERT INTO Structurs VALUES (?,?,?)', data)
+                    self.con.commit()
+                if choice == '1':
+                    data[1] = input('Введите Описание')
+                if choice == '2':
+                    data[2] = input('Введите Штатное расписание')
