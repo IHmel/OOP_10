a1fc4066a32fc360d4579cd6728aa1859b77ff37
IHmel <pro100.hmel@gmail.com>
2023-10-02 14:17:38 +0300

    изменено:      Lab2/Lab2.py 	новый файл:    mydb.db

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -13,27 +13,27 @@ class Database():
                 flag = False
                 
             if choice == '1':
-                choice1 = input('1 - Удаление пользователя'+ '\n2 - Удаление структуры'+'\n')
+                choice1 = input('1 - Добавление пользователя'+ '\n2 - Добавление структуры'+'\n')
                 if choice1 == '1':
                     self.add_user()
                 elif choice1 == 2:
                     self.add_structur()
             if choice == '2':
-                choice1 = input('1 - Добавление пользователя'+ '\n2 - Добавление структуры'+'\n')
+                choice1 = input('1 - Удаление пользователя'+ '\n2 - Удаление структуры'+'\n')
                 if choice1 == '1':
                     self.delete_user(0)
                 elif choice1 == 2:
                     self.add_structur()
             if choice == '3':
-                choice1 = input('1 - Поиск пользователя'+ '\n2 - Поиск структуры'+'\n')
+                choice1 = input('1 - Поиск пользователя'+ '\n2 - Показать краткую информацию о всех пользователях'+ '\n3 - Поиск структуры'+'\n')
                 if choice1 == '1':
                     self.find_user()
-                elif choice1 == 2:
-                    self.add_structur()
+                elif choice1 == '2':
+                    self.find_all_users()
             
     
     def __init__(self):
-        self.con = sqlite3.connect("metanit.db")
+        self.con = sqlite3.connect("mydb.db")
         cur = self.con.cursor()
         cur.execute('''
                     CREATE TABLE IF NOT EXISTS Users (
@@ -100,31 +100,58 @@ class Database():
                 if choice == '2':
                     data[2] = input('Введите Штатное расписание')
                     
-    def find_user(self):
+    def find_all_users(self):
         cur = self.con.cursor()
-        print('Введите СНИЛС нового сотрудника или 0 для отмены')
-        snils = input()
-        if snils == '0':
-            print('Отменено')
-        else:
-            cur.execute('SELECT СНИЛС WHERE СНИЛС = ?', (snils))
-            results = cur.fetchall()
-            for row in results:
-                print(row)
-            while flag:
-                choice = input('Выберете дальнейшие дествия'+ '\n1 - Удаление данных о пользователе'+'\n2 - Изменения данных о пользователе'+'\n0 - Для выхода'+'\n')
-                if choice == '0':
-                    flag = False
-                    self.con.commit()
-                if choice == '1':
-                    self.con.commit()
-                    self.delete_user(snils)
-                    flag = False
-                if choice == '2':
-                    self.con.commit()
-                    self.change_user(snils)
-                    flag = False
+        print('info')
+        cur.execute('SELECT ФИО, Год рождения, СНИЛС FROM Users')
+        results = cur.fetchall()
+        for row in results:
+            print(row)
 
+    def find_user(self, all):
+        cur = self.con.cursor()
+        flag = True
+        while flag:
+            choice1 = input('1 - По ФИО'+ '\n2 - По снилс'+'\n0 - Для отмены'+'\n')
+            if choice1 == '0':
+                flag = False
+                print('Отменено')
+            elif choice1 == '2':
+                print('Введите СНИЛС сотрудника')
+                snils = input()
+                cur.execute('SELECT * FROM Users WHERE СНИЛС = ?', (snils,))
+                results = cur.fetchall()
+                row =results[0]
+                print(row)
+                self.next(snils)
+                flag = False
+            elif choice1 == '1':
+                print('Введите ФИО сотрудника')
+                fio = input()
+                cur.execute('SELECT * FROM Users WHERE ФИО = ?', (fio,))
+                results = cur.fetchall()
+                row =results[0]
+                snils=row[5]
+                print(row)
+                self.next(snils)
+                flag = False
+                    
+    def next(self,snils):
+        flag = True
+        while flag:
+            choice = input('Выберете дальнейшие дествия'+ '\n1 - Удаление данных о пользователе'+'\n2 - Изменения данных о пользователе'+'\n0 - Для выхода'+'\n')
+            if choice == '0':
+                flag = False
+                self.con.commit()
+            if choice == '1':
+                self.delete_user(snils)
+                self.con.commit()
+                flag = False
+            if choice == '2':
+                self.change_user(snils)
+                self.con.commit()
+                flag = False
+                        
     def delete_user(self, snils):
         cur = self.con.cursor()
         if snils == '0':
@@ -133,7 +160,7 @@ class Database():
         if snils == '0':
             print('Отменено')
         else:
-            cur.execute('DELETE FROM Users WHERE СНИЛС =?', (snils))
+            cur.execute('DELETE FROM Users WHERE СНИЛС =?', (snils,))
             self.con.commit()
         
     def change_user(self, snils):
@@ -144,11 +171,16 @@ class Database():
         if snils == '0':
             print('Отменено')
         else:
-            cur.execute('SELECT СНИЛС WHERE СНИЛС = ?', (snils))
-            results = cur.fetchall()[0]
+            cur.execute('SELECT * FROM Users WHERE СНИЛС = ?', (snils,))
+            rows = cur.fetchall()
+            results = rows[0]
+            #print(rows)
             data = [results[0], results[1], results[2], results[3], results[4], results[5]]
+            self.delete_user(snils)
+            #print(data)
             flag = True
             while flag:
+                print(data)
                 choice = input('Выберете данные для изменения'+ '\n1 - ФИО'+ '\n2 - Год рождения'+ '\n3 - Должность'+'\n4 - Доля ставки'+ '\n5 - Дата приема'+'\n0 - Для сохранения'+'\n')
                 if choice == '0':
                     flag = False
