e16676eac75f2ccc027af14979e16190c3acb5b4
IHmel <pro100.hmel@gmail.com>
2023-10-26 21:42:06 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Lab3/lab3.py
    	new file:   Lab3/shop.db
    	deleted:    Lab3/users.txt

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -1,24 +1,25 @@
 import hashlib
 import cryptography
 from cryptography.fernet import Fernet
+import sqlite3;
+
 ################################################################
 
 #Класс движка
 
-class aunt:
-    
-    user_list = {}
-    key = str
-    
-    def read_file(self):
-        with open('OOP_10/Lab1/users.txt', 'r') as file: #Читаем файл
-            lines = file.read().splitlines() # read().splitlines()
-        for line in lines: # Проходимся по каждой строчке
-            key,value = line.split(' ') 
-            self.user_list.update({key:value})	 # До       бавляем в словарь
-
+class authenticate:
+    def __init__(self):
+        con = sqlite3.connect('OOP_10/Lab3/shop.db')
+        cur = con.cursor()
+        cur.execute('''
+                    CREATE TABLE IF NOT EXISTS Users (
+                        login TEXT,
+                        pswd_hash TEXT
+                        )
+                        ''')
+        con.commit()
+        
     def cons(self):
-        self.read_file()
         flag = True
         while flag == True:
             choice = input('Выберете опцию из списка'+ '\n1 - Регистрация'+ '\n2 - Вход пользователя'+ '\n0 - Выход из программы'+ '\n')
@@ -28,17 +29,21 @@ class aunt:
                 self.session()
             elif choice == '0':
                 flag = False
-            elif choice == '4':
-                print(self.user_list)
 
-    def write_file(self):
-        with open('OOP_10/Lab1/users.txt','w') as out:
-            for key,val in self.user_list.items():
-                out.write('{} {}\n'.format(key,val))
-    
     def add_user(self):
-        print('Введите Логин:')
-        login = input()
+        con = sqlite3.connect('OOP_10/Lab3/shop.db')
+        cur = con.cursor()
+        check_login = True
+        while check_login:
+            print('Введите Логин:')
+            login = input()
+            cur.execute('SELECT pswd_hash FROM Users WHERE login =? ', (login,))
+            users = cur.fetchall()
+            #print(users)
+            if len(users) == 0:
+                check_login = False
+            else:
+                print('Такой логин уже есть')
         check = False
         while check == False:
             print('Введите пароль:')
@@ -49,19 +54,25 @@ class aunt:
                 check = True
                 hash_pswrd = hashlib.sha256(bytes(pswrd, 'utf-8')).hexdigest()
                 print('Пользователь',login, 'успешно создан!', sep= ' ')
-                self.user_list.update({login:hash_pswrd})
-                self.write_file()
+                sql = 'INSERT INTO Users (login, pswd_hash) values(?, ?)'
+                data = [login, hash_pswrd]
+                cur.execute(sql, data)
+                con.commit()
             else:
                 print('Пароли не совпадают! Попробуйте еще раз')
             
     def session(self):
+        con = sqlite3.connect('OOP_10/Lab3/shop.db')
+        cur = con.cursor()
         print('Введите логин:')
         login = input()
         try:
-            hash_pswrd_ch = self.user_list[login]
             print('Введите пароль:')
             pswrd = input()
             hash_pswrd = hashlib.sha256(bytes(pswrd, 'utf-8')).hexdigest()
+            cur.execute('SELECT pswd_hash FROM Users WHERE login =? ', (login,))
+            data = cur.fetchall()
+            hash_pswrd_ch = data[0][0]
             if hash_pswrd_ch == hash_pswrd:
                 print('Вы успешно вошли в систему!')
                 Session = True
@@ -78,12 +89,16 @@ class aunt:
 
 
     def change_pswd(self, login):
+        con = sqlite3.connect('OOP_10/Lab3/shop.db')
+        cur = con.cursor()
         check_pswd = False
         while check_pswd == False:
             print('Введите старый пароль:')
             old_pswrd = input()
             hash_old_pswrd = hashlib.sha256(bytes(old_pswrd, 'utf-8')).hexdigest()
-            hash_pswrd_ch = self.user_list[login]
+            cur.execute('SELECT pswd_hash FROM Users WHERE login =? ', (login,))
+            data = cur.fetchall()
+            hash_pswrd_ch = data[0][0]
             if hash_pswrd_ch == hash_old_pswrd:
                 check_pswd = True
                 check = False
@@ -95,8 +110,7 @@ class aunt:
                     if pswrd == pswrd1:
                         check = True
                         hash_pswrd = hashlib.sha256(bytes(pswrd, 'utf-8')).hexdigest()
-                        self.user_list.update({login:hash_pswrd})
-                        self.write_file()
+                        cur.execute('UPDATE Users SET pswd_hash = ? WHERE login = ?', (hash_pswrd, login))
                         print('Для пользователя',login, 'пароль успешно изменен!', sep= ' ')
                     else:
                         print('Пароли не совпадают! Попробуйте еще раз')
@@ -105,6 +119,6 @@ class aunt:
 
 
 if __name__ == '__main__':
-    Cons = aunt()
+    Cons = authenticate()
     Cons.cons()
 
