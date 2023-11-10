0b5bffa8879ca04c45df92b7b122a27c9ae0ed6d
IHmel <pro100.hmel@gmail.com>
2023-11-10 12:21:59 +0300

    Пожалуйста, введите сообщение коммита для ваших изменений. Строки, начинающиеся с «#» будут проигнорированы, а пустое сообщение отменяет процесс коммита.
    Текущая ветка: main
    Эта ветка соответствует «origin/main».
    
    Изменения, которые будут включены в коммит:
    	изменено:      Lab5/lab5.py

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -3,73 +3,83 @@ import time
 import random
 import matplotlib.pyplot as plt
 import array
+import string as str
 
-def time_add_experiment(container_type, num_elements):
-    container = container_type
-    start_time = time.time()
-  
-    for i in range(num_elements):
-        container.insert(0, i)
-  
-    end_time = time.time()
-    return end_time - start_time
+class check_time():
+    
+    def __init__(self):
+        self.container_type1 = list()
+        self.name1 = 'ListContainer'
+        self.container_type2 = array.array('Q')
+        self.name2 = 'ArrayContainer'
+        self.container1_time_add = []
+        self.container2_time_add = []
+        self.container1_time_search = []
+        self.container2_time_search = []
+        self.x_values_add = []
+        self.x_values_search = []
+        
+    def time_add_experiment(self, container_type, num_elements):
+        container = container_type
+        start_time = time.time()    
+        for i in range(num_elements):
+            container.insert(0, i)    
+        end_time = time.time()
+        time_add=end_time - start_time
+        search_keys = random.sample(range(container_size), num_searches)
+        start_time = time.time()    
+        for key in search_keys:
+            container.index(key)    
+        end_time = time.time()
+        time_search =end_time - start_time
+        return time_add, time_search
 
-def time_search_experiment(container_type, container_size, num_searches):
-    container = container_type
-    for i in range(container_size):
-        container.insert(0, i)
-  
-    search_keys = random.sample(range(container_size), num_searches)
-    start_time = time.time()
-  
-    for key in search_keys:
-        container.index(key)
-  
-    end_time = time.time()
-    return end_time - start_time
+    def time_search_experiment(self,container_type, container_size, num_searches):
+        container = container_type
+        for i in range(container_size):
+            container.insert(0, i)    
+        search_keys = random.sample(range(container_size), num_searches)
+        start_time = time.time()    
+        for key in search_keys:
+            container.index(key)    
+        end_time = time.time()
+        return end_time - start_time
 
 
-def plot_graphs(container1_time, container2_time, x_values, title1, title2):
-    plt.plot(x_values, container1_time, label="Container 1")
-    plt.plot(x_values, container2_time, label="Container 2")
-    plt.xlabel("Number of Elements")
-    plt.ylabel("Time (s)")
-    plt.title(title1)
-    plt.grid(True)
-    plt.legend()
-    plt.show()
-    plt.plot(x_values, container1_time, label="Container 1")
-    plt.plot(x_values, container2_time, label="Container 2")
-    plt.xlabel("Number of Searches")
-    plt.ylabel("Time (s)")
-    plt.title(title2)
-    plt.grid(True)
-    plt.legend()
-    plt.show()
+    def plot_graphs(self,container1_time, container2_time, x_values, title1, title2):
+        plt.plot(x_values, container1_time, label=self.name1)
+        plt.plot(x_values, container2_time, label=self.name2)
+        plt.xlabel("Number of Elements")
+        plt.ylabel("Time (s)")
+        plt.title(title1)
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+        plt.plot(x_values, container1_time, label=self.name1)
+        plt.plot(x_values, container2_time, label=self.name2)
+        plt.xlabel("Number of Searches")
+        plt.ylabel("Time (s)")
+        plt.title(title2)
+        plt.grid(True)
+        plt.legend()
+        plt.show()
 
-if __name__ == "__main__":
-    container1_time_add = []
-    container2_time_add = []
-    container1_time_search = []
-    container2_time_search = []
-    x_values_add = []
-    x_values_search = []
-  
-    for num_elements in range(1000, 10001, 1000):
-        container1_time = time_add_experiment(list(), num_elements)
-        container2_time = time_add_experiment(array.array('Q'), num_elements)
-  
-        container1_time_add.append(container1_time)
-        container2_time_add.append(container2_time)
-        x_values_add.append(num_elements)
-  
-    for num_searches in range(1000, 10001, 1000):
-        container1_time = time_search_experiment(list(), 10000, num_searches)
-        container2_time = time_search_experiment(array.array('Q'), 10000, num_searches)
-  
-        container1_time_search.append(container1_time)
-        container2_time_search.append(container2_time)
-        x_values_search.append(num_searches)
-  
-    plot_graphs(container1_time_add, container2_time_add, x_values_add, "Time Taken for Adding Elements", "Time Taken for Searching Elements")
-    plot_graphs(container1_time_search, container2_time_search, x_values_search, "Time Taken for Searching Elements", "Time Taken for Searching Elements")
+    def show_plots(self):    
+        for num_elements in range(1000, 10001, 1000):
+            container1_time_add_experiment,container1_time_search_experiment = self.time_experiment(self.container_type1, num_elements)
+            container2_time_add_experiment,container2_time_search_experiment = self.time_experiment(self.container_type2, num_elements)    
+            self.container1_time_add.append(container1_time_add_experiment)
+            self.container2_time_add.append(container2_time_add_experiment)
+            self.container1_time_search.append(container1_time_search_experiment)
+            self.container2_time_search.append(container2_time_search_experiment)
+            self.x_values_add.append(num_elements)    
+         
+        self.plot_graphs(self.container1_time_add, self.container2_time_add, self.x_values_add, "Add", "Search")
+
+def main():
+    do = check_time()
+    do.show_plots()
+    
+    
+if __name__ == '__main__':
+    main()
