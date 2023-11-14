2db9da9dc79b85652a83d58faefe0dd13fac213f
IHmel <pro100.hmel@gmail.com>
2023-11-14 11:42:38 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Lab3/lab3.py
    	modified:   Lab3/shop.db

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -7,7 +7,7 @@ import os
 
 class authenticate:
     def __init__(self):
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         cur.execute('''
                     CREATE TABLE IF NOT EXISTS Users (
@@ -30,7 +30,7 @@ class authenticate:
                 flag = False
 
     def add_user(self):
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         check_login = True
         while check_login:
@@ -62,7 +62,7 @@ class authenticate:
         con.close()
             
     def session(self):
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         print('Введите логин:')
         login = input()
@@ -114,7 +114,7 @@ class authenticate:
             print('Логин не существует!')
 
     def change_pswd(self, login):
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         check_pswd = False
         while check_pswd == False:
@@ -146,7 +146,7 @@ class authenticate:
 
 class shop:
     def __init__(self):
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         cur.execute('''
                     CREATE TABLE IF NOT EXISTS Goods (
@@ -168,7 +168,7 @@ class shop:
         con.close()
     
     def add_product(self):
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         flag = True
         while flag:
@@ -187,7 +187,7 @@ class shop:
         con.close()
 
     def show_product(self): 
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         cur.execute('SELECT * FROM Goods')
         results = cur.fetchall()
@@ -195,7 +195,7 @@ class shop:
             print(*row)
     
     def show_shop_list(self, login): 
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         if login == 'admin':
             cur.execute('SELECT * FROM shop_list ORDER BY user')
@@ -207,7 +207,7 @@ class shop:
         con.close()
     
     def add_to_shop_list(self, login):
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         flag = True
         while flag:
@@ -246,7 +246,7 @@ class shop:
         con.close()
 
     def pay(self,login):
-        con = sqlite3.connect(os.getcwd()+'/Lab3/shop.db')
+        con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         self.show_shop_list(login)
         choice1 = input('Хотите оплатить товар? (д/н)')
