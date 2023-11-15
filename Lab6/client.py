b23eaf59d87663825d4f18364a141951367dc3ce
IHmel <pro100.hmel@gmail.com>
2023-11-15 19:23:17 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Lab3/shop.db
    	modified:   Lab6/client.py
    	modified:   Lab6/server.py

--- Lab6/client.py
+++ Lab6/client.py
@@ -1,11 +1,13 @@
 import socket
- 
-client = socket.socket()            # создаем сокет клиента
-hostname = socket.gethostname()     # получаем хост локальной машины
-port = 12345                        # устанавливаем порт сервера
-client.connect((hostname, port))    # подключаемся к серверу
-data = client.recv(1024)            # получаем данные с сервера
-print("Server sent: ", data.decode()) 
-#client.send(message.encode())       # отправляем сообщение серверу
 
+class client():
+    def __init__(self):
+        self.client = socket.socket()            # создаем сокет клиента
+        hostname = socket.gethostname()     # получаем хост локальной машины
+        port = 12345                        # устанавливаем порт сервера
+        self.client.connect((hostname, port))
+while True:
+    data = client.recv(1024**2)  
+    print("Server sent: ", data.decode()) 
+    client.send(input().encode())          
 client.close()                      # закрываем подключение
--- Lab6/server.py
+++ Lab6/server.py
@@ -1,13 +1,36 @@
 import socket
-from _thread import *
 import hashlib
 import cryptography
 from cryptography.fernet import Fernet
 import sqlite3
 import os
-
-class authenticate:
+################################################################
+class server():
     def __init__(self):
+        self.server = socket.socket()            # создаем объект сокета сервера
+        hostname = socket.gethostname()     # получаем имя хоста локальной машины
+        port = 12345                        # устанавливаем порт сервера
+        self.server.bind((hostname, port))       # привязываем сокет сервера к хосту и порту
+        self.server.listen(5)      
+        self.connect()
+
+    def data_recv(self,connection):
+        data = connection.recv(1024)
+        return data.decode()
+
+    def data_send(self,connection,data):
+        connection.send(data.encode())
+    
+    def connect(self):
+        print("Server running")
+        while True:
+            connection, _ = self.server.accept()     # принимаем клиента
+            auth = authenticate(connection)
+            auth.cons()
+            connection.close()  
+################################################################
+class authenticate(server):
+    def __init__(self, connect):
         con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         cur.execute('''
@@ -18,68 +41,81 @@ class authenticate:
                         ''')
         con.commit()
         con.close()
+        self.connection = connect
+        
+    def cons(self):
+        flag = True
+        while flag == True:
+            self.data_send(self.connection,str('Выберете опцию из списка'+ '\n1 - Регистрация'+ '\n2 - Вход пользователя'+ '\n0 - Выход из программы'+ '\n'))
+            choice = self.data_recv(self.connection)
+            if choice == '1':
+                self.add_user()
+            elif choice == '2':
+                self.session()
+            elif choice == '0':
+                flag = False
 
     def add_user(self):
         con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         check_login = True
         while check_login:
-            print('Введите Логин:')
-            login = input()
+            self.data_send(self.connection,str('Введите Логин:'))
+            login = self.data_recv(self.connection)
             cur.execute('SELECT pswd_hash FROM Users WHERE login =? ', (login,))
             users = cur.fetchall()
-            #print(users)
             if len(users) == 0:
                 check_login = False
             else:
-                print('Такой логин уже есть')
+                self.data_send(self.connection,str('Такой логин уже есть'))
         check = False
         while check == False:
-            print('Введите пароль:')
-            pswrd = input()
-            print('Введите пароль еще раз:')
-            pswrd1 = input()
+            self.data_send(self.connection,str('Введите пароль:'))
+            pswrd = self.data_recv(self.connection)
+            self.data_send(self.connection,str('Введите пароль еще раз:'))
+            pswrd1 = self.data_recv(self.connection)
             if pswrd == pswrd1:
                 check = True
                 hash_pswrd = hashlib.sha256(bytes(pswrd, 'utf-8')).hexdigest()
-                print('Пользователь',login, 'успешно создан!', sep= ' ')
+                self.data_send(self.connection,str('Пользователь',login, 'успешно создан!', sep= ' '))
                 sql = 'INSERT INTO Users (login, pswd_hash) values(?, ?)'
                 data = [login, hash_pswrd]
                 cur.execute(sql, data)
                 con.commit()
             else:
-                print('Пароли не совпадают! Попробуйте еще раз')
+                self.data_send(self.connection,str('Пароли не совпадают! Попробуйте еще раз'))
         con.close()
             
     def session(self):
         con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
-        print('Введите логин:')
-        login = input()
+        self.data_send(self.connection,str('Введите логин:'))
+        login = self.data_recv(self.connection)
         try:
-            print('Введите пароль:')
-            pswrd = input()
+            self.data_send(self.connection,str('Введите пароль:'))
+            pswrd = self.data_recv(self.connection)
             hash_pswrd = hashlib.sha256(bytes(pswrd, 'utf-8')).hexdigest()
             cur.execute('SELECT pswd_hash FROM Users WHERE login =? ', (login,))
             data = cur.fetchall()
             con.close()
             if len(data) == 0:
-                print('Такого пользователя не существует')
+                self.data_send(self.connection,str('Такого пользователя не существует'))
             else:
                 hash_pswrd_ch = data[0][0]
                 if hash_pswrd_ch == hash_pswrd:
-                    print('Вы успешно вошли в систему!')
+                    self.data_send(self.connection,str('Вы успешно вошли в систему!'))
                     Session = True
                     while(Session == True):
-                        Shop = shop()
-                        choice = input('Выберете опцию из списка'+ 
+                        Shop = shop(self.connection)
+                        self.data_send(self.connection,str('Выберете опцию из списка'+ 
                                         '\n0 - Выход'+
                                         '\n1 - Смена пароля'+ 
                                         '\n2 - Список товаров'+
                                         '\n3 - Добавить товар'+
                                         '\n4 - Корзина'+
                                         '\n5 - Добавить в корзину'+
-                                        '\n6 - Оплатить товар\n')
+                                        '\n6 - Оплатить товар\n'))
+                        choice = self.data_recv(self.connection)
                         if choice == '1':
                             self.change_pswd(login)
                         elif choice == '0':
@@ -90,7 +126,7 @@ class authenticate:
                             if login == 'admin':
                                 Shop.add_product()
                             else:
-                                print('Недостаточно прав'+'\n')
+                                self.data_send(self.connection,str('Недостаточно прав'+'\n'))
                         elif choice == '4':
                             Shop.show_shop_list(login)
                         elif choice == '5':
@@ -99,17 +135,17 @@ class authenticate:
                             Shop.pay(login)
                             
                 else:
-                    print('Неверный пароль')
+                    self.data_send(self.connection,str('Неверный пароль'))
         except KeyError:
-            print('Логин не существует!')
+            self.data_send(self.connection,str('Логин не существует!'))
 
     def change_pswd(self, login):
         con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         check_pswd = False
         while check_pswd == False:
-            print('Введите старый пароль:')
-            old_pswrd = input()
+            self.data_send(self.connection,str('Введите старый пароль:'))
+            old_pswrd = self.data_recv(self.connection)
             hash_old_pswrd = hashlib.sha256(bytes(old_pswrd, 'utf-8')).hexdigest()
             cur.execute('SELECT pswd_hash FROM Users WHERE login =? ', (login,))
             data = cur.fetchall()
@@ -118,24 +154,25 @@ class authenticate:
                 check_pswd = True
                 check = False
                 while check == False:
-                    print('Введите пароль:')
-                    pswrd = input()
-                    print('Введите пароль еще раз:')
-                    pswrd1 = input()
+                    self.data_send(self.connection,str('Введите пароль:'))
+                    pswrd = self.data_recv(self.connection)
+                    self.data_send(self.connection,str('Введите пароль еще раз:'))
+                    pswrd1 = self.data_recv(self.connection)
                     if pswrd == pswrd1:
                         check = True
                         hash_pswrd = hashlib.sha256(bytes(pswrd, 'utf-8')).hexdigest()
                         cur.execute('UPDATE Users SET pswd_hash = ? WHERE login = ?', (hash_pswrd, login))
                         con.commit()
-                        print('Для пользователя',login, 'пароль успешно изменен!', sep= ' ')
+                        self.data_send(self.connection,str('Для пользователя',login, 'пароль успешно изменен!', sep= ' '))
                     else:
-                        print('Пароли не совпадают! Попробуйте еще раз')
+                        self.data_send(self.connection,str('Пароли не совпадают! Попробуйте еще раз'))
             else:
-                print('Неверный пароль')
+                self.data_send(self.connection,str('Неверный пароль'))
         con.close()
 
-class shop:
-    def __init__(self):
+class shop(server):
+    def __init__(self, connect):
+        self.connection = connect
         con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         cur.execute('''
@@ -162,16 +199,18 @@ class shop:
         cur = con.cursor()
         flag = True
         while flag:
-            print('Введите товар, коллисество и стоимость через пробел: ')
-            data = input().split()
-            print(data)
-            choice = input('Добавить товар? (д/н)'+'\n')
+            self.data_send(self.connection,str('Введите товар, коллисество и стоимость через пробел: '))
+            data = self.data_recv(self.connection).split()
+            self.data_send(self.connection,str(data))
+            self.data_send(self.connection,str('Добавить товар? (д/н)'+'\n'))
+            choice = self.data_recv(self.connection)
             if choice == 'Н' or choice == 'N' or choice == 'n' or choice == 'н':
-                print('Отменено')
+                self.data_send(self.connection,str('Отменено'))
             else:
                 cur.execute('INSERT INTO Goods VALUES (?,?,?)', [str(data[0]), int(data[1]), float(data[2])])
                 con.commit()
-            choice1 = input('Хотите добавить еще товар? (д/н)')
+            self.data_send(self.connection,str('Хотите добавить еще товар? (д/н)'))
+            choice1 = self.data_recv(self.connection)
             if choice1 == 'Н' or choice1 == 'N' or choice1 == 'n' or choice1 == 'н':
                 flag = False
         con.close()
@@ -181,8 +220,12 @@ class shop:
         cur = con.cursor()
         cur.execute('SELECT * FROM Goods')
         results = cur.fetchall()
+        massage =''
         for row in results:
-            print(*row)
+            for r in row:
+                massage = massage + str(r)+' '
+            massage = massage +'\n'
+        self.data_send(self.connection,str(massage))
     
     def show_shop_list(self, login): 
         con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
@@ -192,8 +235,12 @@ class shop:
         else:
             cur.execute('SELECT name, count, cost, oplata FROM shop_list where user = ?', [login])
         results = cur.fetchall()
+        massage =''
         for row in results:
-            print(*row)
+            for r in row:
+                massage = massage + str(r)+' '
+            massage = massage +'\n'
+        self.data_send(self.connection,str(massage))
         con.close()
     
     def add_to_shop_list(self, login):
@@ -202,24 +249,23 @@ class shop:
         flag = True
         while flag:
             self.show_product()
-            print('Введите товар, коллисество для заказа: ')
-            data = input().split()
+            self.data_send(self.connection,str('Введите товар, коллисество для заказа: '))
+            data = self.data_recv(self.connection).split()
             name_zakaz = str(data[0]) #name
-            #print(data[0], type(data[0]))
             kolvo_zakaz = int(data[1]) #kolvo v zakaz
             cur.execute('SELECT * FROM Goods where name =?', (name_zakaz,))
             result = cur.fetchone()
             kolvo = result[1] #kolvo v magazine
             cost = result[2] #cost
             if kolvo >= kolvo_zakaz:
-                print('Товар добавлен в корзину')
+                self.data_send(self.connection,str('Товар добавлен в корзину'))
                 if kolvo == kolvo_zakaz:
                     cur.execute('DELETE FROM Goods where name =?', (name_zakaz,))
                 else:
                     cur.execute('UPDATE Goods SET count=? where name =?', [kolvo - kolvo_zakaz, name_zakaz])
                 cur.execute('SELECT * FROM shop_list WHERE user =? AND name =? AND oplata = ?', [login, name_zakaz, 'Не оплачен'])
                 tovar = cur.fetchall()
-                #print(users)
+                #self.data_send(self.connection,str(users)
                 if len(tovar) == 0:
                     sql = 'INSERT INTO shop_list (user, name, count, cost, oplata) values(?, ?, ?, ?, ?)'
                     data = [login, name_zakaz, kolvo_zakaz, cost, 'Не оплачен']
@@ -229,8 +275,9 @@ class shop:
                     cur.execute('UPDATE shop_list SET count = ? where name =?', [real_kolvo + kolvo_zakaz, name_zakaz])
                 con.commit()
             else:
-                print('Товара недостаточно на складе')
-            choice1 = input('Хотите добавить в корзину еще товар? (д/н)')
+                self.data_send(self.connection,str('Товара недостаточно на складе'))
+            self.data_send(self.connection,str('Хотите добавить в корзину еще товар? (д/н)'))
+            choice1 = self.data_recv(self.connection)
             if choice1 == 'Н' or choice1 == 'N' or choice1 == 'n' or choice1 == 'н':
                 flag = False
         con.close()
@@ -239,45 +286,12 @@ class shop:
         con = sqlite3.connect(os.getcwd()+'/OOP_10/Lab3/shop.db')
         cur = con.cursor()
         self.show_shop_list(login)
-        choice1 = input('Хотите оплатить товар? (д/н)')
+        self.data_send(self.connection,str('Хотите оплатить товар? (д/н)'))
+        choice1 = self.data_recv(self.connection)
         if choice1 == 'Y' or choice1 == 'y' or choice1 == 'Д' or choice1 == 'д':
             cur.execute('UPDATE shop_list SET oplata =?  where user =?', ['Оплачен',login])
             con.commit()
         con.close()
-    
-    
-class Server():
-    
-    def __init__(self):
-        self.socket_server = socket.socket()            # создаем объект сокета сервера
-        hostname = socket.gethostname()     # получаем имя хоста локальной машины
-        port = 12345                        # устанавливаем порт сервера
-        self.socket_server.bind((hostname, port))       # привязываем сокет сервера к хосту и порту
-        self.socket_server.listen(5) 
-        self.run_server()
-    
-    def client_check(con):
-        data = con.recv(1024)           # получаем данные от клиента
-        message = data.decode()
-        if message == '1':
-            Auth = authenticate()
-            Auth.add_user()
-        elif message == '2':
-            Auth = authenticate()
-            Auth.session()
-        elif message == '0':
-            con.close() 
-
-    def server_send(self, con, message):
-        message = message[::-1]         # инвертируем строку
-        con.send(message.encode())      # отправляем сообщение клиенту
-        con.close()                     # закрываем подключение
-
-    def run_server(self):
-        print("Server running")
-        while True:
-            client, _ = self.socket_server.accept()     # принимаем клиента
-            start_new_thread(self.server_send, (client,)) 
 
 if __name__ == '__main__':
-    server = Server()
+    Server =server()
